e87ddac068e356093b31bf5bbd99557ee286a2a5
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-14 21:13:22 +0530

    feat:made the backend working with proper endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
-from .schemas import schemas
-import pickle
+from schemas import schemas
 
-with open('models/spam_model.pkl','rb') as f:
-    model=pickle.load(f)
-with open('models/tfidf_vectorizer.pkl','rb') as f:
-    vectorize=pickle.load(f)
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+model_path = os.path.join(BASE_DIR, "models", "spam_model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "models", "tfidf_vectorizer.pkl")
+
+
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
+
 
 app =FastAPI()
 
 
-@app.post('/prefict')
+@app.post('/predict')
 def predict(email:schemas.Email):
-    X=vectorize.transform([email.text])
-    pred=model.predict(X)
+    X=vectorizer.transform([email.text])
+    pred=model.predict(X)[0] #as the model gives an array u are accessing the first element
     return{"prediction":'spam' if pred else "not spam"}
