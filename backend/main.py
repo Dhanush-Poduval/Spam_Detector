ca6e7404cc0dac68c48f3fb3dd87ee9585318753
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-14 19:56:18 +0530

    feat:made the backend of the websitr and integrated with the model

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from .schemas import schemas
+import pickle
+
+with open('models/spam_model.pkl','rb') as f:
+    model=pickle.load(f)
+with open('models/tfidf_vectorizer.pkl','rb') as f:
+    vectorize=pickle.load(f)
 
 app =FastAPI()
 
-@app.get('/details')
-def values():
-    return 'Spider man is the best'
+
+@app.post('/prefict')
+def predict(email:schemas.Email):
+    X=vectorize.transform([email.text])
+    pred=model.predict(X)
+    return{"prediction":'spam' if pred else "not spam"}
